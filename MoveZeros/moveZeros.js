5f9a5877d2a973fc4e14eaedd73357d91725c248
Mark Suzuki <markdsuzuki@gmail.com>
2018-06-21 13:48:50 -0700

    alternative solution moveZeros

--- MoveZeros/moveZeros.js
+++ MoveZeros/moveZeros.js
@@ -23,6 +23,36 @@ function moveZeroes(list){
   return list 
 }
 
+// ALTERNATIVE SOLUTION
+/**
+ * Strategy:
+ * - keep pointer, `insertIdx`, at first zero
+ * - loop over each num in nums
+ *   - at val that isn't zero, place it at `insertIdx`
+ *     - then increment `inesertPos` and repeat
+ * - after loop, `insertIdx` is at index where all zeroes should go
+ */
+
+function moveZeroes(nums) {
+  let insertIdx = 0;
+
+  // iterate thru arr, swapping out zeroes for other numbers
+  for (let num of nums) {
+    if (num !== 0) {
+      nums[insertIdx] = num;
+      insertIdx++;
+    }
+  }
+
+  // put zeroes back in, starting at insert position until end
+  while (insertIdx < nums.length) {
+    nums[insertIdx] = 0;
+    insertIdx++;
+  }
+
+  return nums;
+}
+
 moveZeroes([0,1,0,3,12]) // [1,3,12,0,0]
 moveZeroes([1,3,10,2]) // [1,3,10,2]
 moveZeroes([4,1,2,0,0,1,2,1,0]) // [4,1,2,1,2,1,0,0,0]
