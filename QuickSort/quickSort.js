00ddd549691ae932f9f54ac1fb487ccd9fc7c625
Mark Suzuki <markdsuzuki@gmail.com>
2018-09-13 17:19:24 -0700

    updates to pivot function

--- QuickSort/quickSort.js
+++ QuickSort/quickSort.js
@@ -1,7 +1,7 @@
 function pivot(list, start=0, end=list.length-1) {
   let pivotIdx = 0 // keeps track of where "pivot Value needs to end up at once we get to the end"
   let check = start // list[check] is the pivot value that is to compare all values against
-  for(let i = 1; i < list.length; i++) {
+  for(let i = start + 1; i <= end; i++) { // use start and end in for loop otherwise it will only work once with recursion
     if(list[check] > list[i]) { // check if current value is less the "pivot value"
       pivotIdx++ // increment(pivotIdx) first before swaping otherwise you'll swap pivotValue which is last to get swapped 
       [list[i], list[pivotIdx]] = [list[pivotIdx],list[i]]
